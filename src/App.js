4bc47b7895c638f4ff351eb76f95eae9c9aad107
Atul Singh <atul.ks1998@gmail.com>
2021-07-16 23:09:51 +0530

    map function added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import Card from "./components/card";
 import contacts from "./components/contact";
 import "./style.css";
+function main(contacts) {
+  return(
+    <Card
+        name={contacts.name}
+        img={contacts.imgURL}
+        tel={contacts.phone}
+        email={contacts.email}
+    />
+  );
+}
 function App() {
   return (
     <div className="App">
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(main)}
     </div>
   );
 }
